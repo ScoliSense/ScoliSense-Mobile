539dc7d6a62eba63289ce2389640d6b134130df5
OsmanEmreKaya <128143456+OsmanEmreKaya@users.noreply.github.com>
2025-04-17 21:10:55 +0300

    last version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,7 @@ import 'package:skolyozmobil/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(isLoggedIn: false));
-
+    await tester.pumpWidget(MyApp());
 
 
     // Verify that our counter starts at 0.
